03056ab2f718d612c554473ec7fdc238214cfe53
NinthBurn <aflorin149@yahoo.com>
2024-11-21 00:06:16 +0200

    refactor everything into classes, fix prime generator (again)

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 #### TODO
-* refactor everything into a proper class
-* better prime number generation
+* refactor everything into a proper class ✓
+* better prime number generation ✓
 * implement the following:
-  * Setting: the alphabet will have 27 characters, the blank and the 26 letters of the English alphabet.
-  * Generates a public key and a private key. The public key will be randomly generated in the required interval.
+  * Setting: the alphabet will have 27 characters, the blank and the 26 letters of the English alphabet. ✓
+  * Generates a public key and a private key. The public key will be randomly generated in the required interval. ✓
 	* Plaintext message units are blocks of k letters, whereas ciphertext message units are blocks of l letters. The plaintext is completed with blanks, when necessary.
 	  * We must have 27<sup>k</sup> < n < 27<sup>l</sup>
-  * Using the public key, encrypts a given plaintext. 
+  * Using the public key, encrypts a given plaintext. ✓
     * there will be a plaintext validation - check that all characters of initial message belong to the alphabet
-  * Using the private key, decrypts a given ciphertext. 
+  * Using the private key, decrypts a given ciphertext. ✓
     * there will be a ciphertext validation - ?
--- lab4.py
+++ lab4.py
@@ -4,154 +4,190 @@ from math import sqrt, ceil
 # lab4 - ex. 3
 # lab5 - ex. 5
 
-def prime_factors(n):
-    factors = []
+class PrimeUtils:
+    @classmethod   
+    def miller_test(cls, t, n):
+        a = 2 + random.randint(1, n - 4)
 
-    while n % 2 == 0:
-        factors.append(2)
-        n //= 2
-    
-    for i in range(3, ceil(sqrt(n)), 2):
-        while n % i == 0:
-            factors.append(i)
-            n //= i
-    
-    # if n is a prime number
-    if n > 2:
-        factors.append(n)
-    
-    return factors
+        x = pow(a, t, n)
 
-def find_generator(p):
-    phi = p - 1
-    factors = prime_factors(phi)
-
-    # Test candidates for g
-    for g in range(2, p):
-        is_generator = True
-        for q in factors:
-            # Check if g^(phi/q) =/= 1 (mod p)
-            if pow(g, phi // q, p) == 1:
-                is_generator = False
-                break
-
-        if is_generator:
-            return g
-        
-def MillerTest(t, n):
-    a = 2 + random.randint(1, n - 4)
-
-    x = pow(a, t, n)
-
-    if x == 1 or x == n - 1:
-        return True
-
-    while t != n - 1:
-        x = (x * x) % n
-        t *= 2
-
-        if x == 1:
-            return False
-        if x == n - 1:
+        if x == 1 or x == n - 1:
             return True
 
-    return False
+        while t != n - 1:
+            x = (x * x) % n
+            t *= 2
 
-# Miller-Rabin test
-def isPrime(n, k):
-    if n <= 1 or n == 4:
+            if x == 1:
+                return False
+            if x == n - 1:
+                return True
+
         return False
-    if n <= 3:
+
+    # Miller-Rabin test
+    @classmethod   
+    def is_prime(cls, n, k):
+        if n <= 1 or n == 4:
+            return False
+        if n <= 3:
+            return True
+
+        t = n - 1
+        while t % 2 == 0:
+            t //= 2
+
+        for _ in range(k):
+            if not cls.miller_test(t, n):
+                return False
+
         return True
 
-    t = n - 1
-    while t % 2 == 0:
-        t //= 2
+    @classmethod   
+    def prime_factors(cls, n):
+        factors = []
 
-    for _ in range(k):
-        if not MillerTest(t, n):
-            return False
+        while n % 2 == 0:
+            factors.append(2)
+            n //= 2
+        
+        for i in range(3, ceil(sqrt(n)), 2):
+            while n % i == 0:
+                factors.append(i)
+                n //= i
+        
+        # if n is a prime number
+        if n > 2:
+            factors.append(n)
+        
+        return factors
 
-    return True
+    @classmethod   
+    def find_generator(cls, p):
+        phi = p - 1
+        factors = cls.prime_factors(phi)
 
-# good for 4 letter words
-def generate_prime_hardcoded():
-    return 923501
+        # Test candidates for g
+        for g in range(2, p):
+            is_generator = True
+            for q in factors:
+                # Check if g^(phi/q) =/= 1 (mod p)
+                if pow(g, phi // q, p) == 1:
+                    is_generator = False
+                    break
 
-def generate_prime_bits(bits, prime_trial_count):
-    while(1):
-        p = random.randint( 2**(bits-1), 2**(bits) - 1 )
+            if is_generator:
+                return g
 
-        while(p % 2 != 0 and not isPrime(p, prime_trial_count)):
-            p = random.randint( 2**(bits-1), 2**(bits) - 1 )
+    @classmethod   
+    def generate_prime_bits(cls, bits, prime_trial_count):
+        while(1):
+            p = random.randint( 2**(bits-1) + 1, 2**(bits) - 1 )
 
-        return p
-                      
-def generate_prime_interval(min_value, max_value, prime_trial_count):
-    while(1):
-        p = random.randint( min_value + 1, max_value )
+            while(p % 2 == 0 and not cls.is_prime(p, prime_trial_count)):
+                p = random.randint( 2**(bits-1) + 1, 2**(bits) - 1 )
 
-        while(p % 2 != 0 and not isPrime(p, prime_trial_count)):
+            return p
+
+    @classmethod   
+    def generate_prime_interval(cls, min_value, max_value, prime_trial_count):
+        while(1):
             p = random.randint( min_value + 1, max_value )
 
-        return p
-    
-def generate_keys(min_size_power):
-    p = generate_prime_interval(27 ** min_size_power, 27 ** (min_size_power + 1), 4)
-    g = find_generator(p)
+            while(p % 2 == 0 and not cls.is_prime(p, prime_trial_count)):
+                p = random.randint( min_value + 1, max_value )
 
-    a = random.randint(1, p - 2)
+            return p
+            
+class MessageConverter:
+    def __init__(self, chars: str) -> None:
+        # character to value mapping
+        self.chars = {}
 
-    return ((p, g, pow(g, a, p)), a)
+        # value to character mapping
+        self.char_vals = {}
 
-chars = {' ': 0}
-for i in range(ord('a'), ord('z') + 1):
-    chars[chr(i)] = i - 96
+        for i in range(0, len(chars)):
+            self.chars[chars[i]] = i
+            self.char_vals[i] = chars[i]
 
-def encrypt(key, message):
-    length = len(message)
+        self.length = len(chars)
 
-    converted_message = chars[message[length - 1]]
-    pos = 27
+    def to_num(self, message_string: str) -> int:
+        str_len = len(message_string)
 
-    for index in range(length - 2, -1, -1):
-        converted_message += pos * chars[message[index]]
-        pos *= 27
-         
-    p, g, ga = key
+        converted_message = self.chars[message_string[str_len - 1]]
+        current_pos = self.length
 
-    k = random.randint(1, p)
-    alpha = pow(g, k, p)
-    beta = converted_message * pow(ga, k, p)
+        for index in range(str_len - 2, -1, -1):
+            converted_message += current_pos * self.chars[message[index]]
+            current_pos *= self.length
 
-    ciphertext = (alpha, beta)
+        return converted_message
 
-    return ciphertext
+    def to_str(self, message_num: int) -> str:
+        result = ""
 
-def message_to_str(num):
-    result = ""
+        if message_num == 0:
+            return " "
+        
+        while message_num > 0:
+            char_index = message_num % self.length
+            message_num //= self.length
 
-    while(num > 0):
-        r = num % 27
-        num //= 27
+            result = self.char_vals[char_index] + result
 
-        if r == 0:
-            result = " " + result
-        else: result = chr(r + 96) + result         
+        return result
 
-    return result
+    # split string into equal chunks of given size
+    def split_string_into_chunks(self, string: str, size: int):
+        return [string[i:i + size] for i in range(0, len(string), size)]
 
-def decrypt(key, ciphertext):
-    public, a = key
-    p, g, ga = public
-    alpha, beta = ciphertext
+class ElGamalEncryption:
+    def __init__(self, plaintext_block_length, ciphertext_block_length, alphabet):
+        self.plaintext_block_length = plaintext_block_length
+        self.ciphertext_block_length = ciphertext_block_length
+        self.message_converter = MessageConverter(alphabet)
 
-    # TODO: figure out why alpha is sometimes invertible (the program crashes 🙃)
-    m = pow(alpha, -a, p) * beta % p
-    
-    return message_to_str(m)
+    def generate_keys(self):
+        p = PrimeUtils.generate_prime_interval(
+            27 ** self.plaintext_block_length + 1, 
+            27 ** self.ciphertext_block_length, 4
+        )
 
-message = "hello"
-keys = generate_keys(len(message))
-encrypted = encrypt(keys[0], message)
-print(decrypt(keys, encrypted))
+        g = PrimeUtils.find_generator(p)
+        a = random.randint(1, p - 2)
+
+        return ((p, g, pow(g, a, p)), a)
+
+    def encrypt(self, key, message):
+        length = len(message)
+
+        converted_message = self.message_converter.to_num(message)
+            
+        p, g, ga = key
+
+        k = random.randint(1, p)
+        alpha = pow(g, k, p)
+        beta = converted_message * pow(ga, k, p)
+
+        ciphertext = (alpha, beta)
+
+        return ciphertext
+
+    def decrypt(self, key, ciphertext):
+        public, a = key
+        p, g, ga = public
+        alpha, beta = ciphertext
+
+        m = pow(alpha, -a, p) * beta % p
+        
+        return self.message_converter.to_str(m)
+
+if __name__ == "__main__":
+    encrypter = ElGamalEncryption(11, 12, " abcdefghijklmnopqrstuvwxyz")
+    message = "hello world"
+
+    keys = encrypter.generate_keys()
+    encrypted_message = encrypter.encrypt(keys[0], message)
+    print(encrypter.decrypt(keys, encrypted_message))
